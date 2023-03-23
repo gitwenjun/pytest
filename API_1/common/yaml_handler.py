de9dfba31379360477213c86fd17d7195a17b7c6
gitwenjun <946943326@qq.com>
2023-03-23 19:47:17 +0800

    增加读写extract文件方法

--- API_1/common/yaml_handler.py
+++ API_1/common/yaml_handler.py
@@ -1,5 +1,8 @@
 import yaml
+
+
 class YamlHandler:
+
     def read_yaml(self,fileName):
         with open(fileName,"r",encoding="utf-8") as f:
             data = yaml.load(stream=f,Loader=yaml.FullLoader)
@@ -13,3 +16,15 @@ class YamlHandler:
     def clean_yaml(self,fileName):
         with open(fileName,"w",encoding="utf-8") as f:
             f.truncate()
+
+    def read_extrct_yaml(self,key):
+        fileName = "../data/read_extract.yaml"
+        with open(fileName,"r",encoding="utf-8") as f:
+            data = yaml.load(stream=f,Loader=yaml.FullLoader)
+            return data[key]
+
+    def write_extract_yaml(self,data):
+        fileName = "../data/session.yaml"
+        with open(fileName,"a",encoding="utf-8") as f:
+            data = yaml.dump(data=data,stream=f,allow_unicode=True)
+            return data
